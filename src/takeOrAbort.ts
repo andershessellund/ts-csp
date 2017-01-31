3ac8bc95fe1142e5b6022458d82447d90ea4d33d
Anders Hessellund Jensen <anders@andershessellund.dk>
2017-01-31 11:05:11 +0100

    Minor fixes

--- src/takeOrAbort.ts
+++ src/takeOrAbort.ts
@@ -1,8 +1,8 @@
 import * as Promise from 'bluebird';
 
-import {Source, OperationType, SelectTakeResult, TakeOperation, Selectable} from "../src/api";
-import {Signal} from "../src/Signal";
-import {selectOrAbort} from "../src/selectOrAbort";
+import {Source, OperationType, SelectTakeResult, TakeOperation, Selectable} from "./api";
+import {Signal} from "./Signal";
+import {selectOrAbort} from "./selectOrAbort";
 export const takeOrAbort = (abortSignal: Signal, source: Source & Selectable): Promise<any> => {
     const selectSpec: TakeOperation[] = [
         { ch: source, op: OperationType.TAKE}
